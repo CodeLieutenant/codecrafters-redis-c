449577a7d0d66e17dca177f5e3625bbbc5c46e6d
Dusan Malusev <dusan@dusanmalusev.dev>
2024-05-20 23:07:42 +0200

    change build script

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 set -xe
-cmake --preset ReleaseCodecrafters -S .
+cmake --preset ReleaseCodecrafters -S . -B build/ReleaseCodecrafters
 cmake --build build/ReleaseCodecrafters
 
 exec ./build/ReleaseCodecrafters/server "$@"
