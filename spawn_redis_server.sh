3ef1ac1d964e1c15011ab284d8ffd9ee9fa44b03
Dusan Malusev <dusan@dusanmalusev.dev>
2024-05-21 00:22:43 +0200

    using mimalloc
    Signed-off-by: Dusan Malusev <dusan@dusanmalusev.dev>

--- spawn_redis_server.sh
+++ spawn_redis_server.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 set -xe
-cmake -S . -B build/ReleaseCodecrafters -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+cmake -S . -B build/ReleaseCodecrafters -DCMAKE_BUILD_TYPE=Release "-DCMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
+"
 cmake --build build/ReleaseCodecrafters
 
 exec ./build/ReleaseCodecrafters/server "$@"
